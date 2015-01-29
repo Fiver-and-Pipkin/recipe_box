a92028e10e71e8e21e0bf323e32370aeb6cbda66
Debbie Cravey & Jacqueline McVey <student@epicodus.com>
2015-01-29 13:34:32 -0800

    add initial Sinatra files

--- app.rb
+++ app.rb
@@ -5,3 +5,32 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 get('/') do
   erb(:index)
 end
+
+
+get('/entree') do
+  under construction
+  erb(:entree)
+end
+
+get('/side') do
+under construction
+  erb(:side)
+end
+
+
+get('/dessert') do
+  under construction
+  erb(:dessert)
+end
+
+get('/add_recipe') do
+  @recipes = Recipe.all()
+  erb(:add_recipe)
+end
+
+post('/add_recipe') do
+  recipe_name = params.fetch("recipe_name")
+  @recipes = Recipe.create({:recipe_name => recipe_name})
+  redirect('/add_recipe')
+  erb(:add_recipe)
+end
